29811339dc0a66a464eedc08ba077d477a029178
Galib <rj.rafi35@gmail.com>
2023-04-03 23:24:41 +0600

    nodemon and server side rendering

---
+++ start_server.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+set PROJECT_PATH=D:\Software\Projects\Laravel\Restaurant_Management
+set XAMPP_PATH=C:\xampp
+
+set PATH=%PATH%;%XAMPP_PATH%\php;%XAMPP_PATH%\php\ext
+
+rem Open PowerShell in the project directory
+start powershell.exe -NoExit -Command "cd %PROJECT_PATH% ; %XAMPP_PATH%\php\php.exe artisan serve"
+
+rem Open a second PowerShell window in the project directory
+start powershell.exe -NoExit -Command "cd %PROJECT_PATH% ; npm run dev"
+
+rem Open a second PowerShell window in the project directory
+start powershell.exe -NoExit -Command "cd %PROJECT_PATH% ; nodemon server.js"
+
+start firefox.exe -new-tab "http://localhost:8000/login"
+
+endlocal
